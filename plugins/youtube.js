128dfdf204252e3267e9006e028b9cdc01941dc7
logasland <62803820+logasland@users.noreply.github.com>
2022-10-14 17:24:06 +0700

    Create youtube.js

---
+++ plugins/youtube.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+	if (!text) throw `*Link Youtube nya mana?*\n\n*contoh:https://youtu.be/oa04wbLyENo`
+	let res = await fetch(`https://itztobz.me/api/youtube-mp4?url=` + text)
+	let json = await res.json()
+
+conn.sendFile(m.chat, json.result[0].download, 'fb.mp4', author, m)
+}
+handler.help = ['yt'].map(v => v + ' <url>')
+
+handler.tags = ['downloader']
+
+handler.limit = true
+handler.group = true
+
+handler.command = /^(yt|ytmp4|ytv)$/i
