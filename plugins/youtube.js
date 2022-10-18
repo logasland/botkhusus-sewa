dc8e0eb29d19ac9420110f6cacb8cfa647f192a0
logasland <62803820+logasland@users.noreply.github.com>
2022-10-18 23:25:54 +0700

    Update youtube.js

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -2,10 +2,17 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 	if (!text) throw `*Link Youtube nya mana?*\n\n*contoh:https://youtu.be/oa04wbLyENo`
-	let res = await fetch(`https://itztobz.me/api/youtube-mp4?url=` + text )
+	let res = await fetch(`https://itztobz.me/api/youtube-mp4?url=` + text)
 	let json = await res.json()
 
-conn.sendFile(m.chat, json.result[0].download, 'fb.mp4', `*Title : ${json.title}*\n\n_${wm}_`, m)
+conn.sendMessage(m.chat, {
+        video: await(await fetch(json.result[0].download)).buffer(),
+        caption: `*Title :* ${json.title}\n\n\n_${wm}_`,
+        buttons: [
+          {buttonId: `.yta ${text}`, buttonText: {displayText: 'AUDIO (MP3)'}, type: "RESPONSE"},
+        ],
+        headerType: 'VIDEO'
+  }, { quoted: m })
 }
 
 handler.help = ['yt'].map(v => v + ' <url>')
