bc1217b5b27d1356723a77a94609cf5594cb7167
logasland <62803820+logasland@users.noreply.github.com>
2022-10-19 09:15:37 +0700

    Update cecan.js

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -2,7 +2,7 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("cecan","ullzang girl", "cewe cantik", "hijab cantik", "korean girl", "remaja cantik", "cewek korea", "cewek jepang").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+	scrap.pinterest("cewe manis","hot girl", "cewe cantik", "hijab cantik", "korean girl", "remaja cantik", "cewek korea", "cewek jepang").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
    }
     
 handler.help = ['cecan']
