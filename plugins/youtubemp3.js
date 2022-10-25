cc4967c686ffe751fb79ff0140a8c5e2b5b7f9e2
logasland <62803820+logasland@users.noreply.github.com>
2022-10-25 22:35:58 +0700

    Update youtubemp3.js

--- plugins/youtubemp3.js
+++ plugins/youtubemp3.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 	if (!text) throw `*Link Youtube nya mana?*\n\n*contoh:https://youtu.be/oa04wbLyENo`
 	let res = await fetch(`https://itztobz.me/api/youtube-mp3?url=` + text)
 	let json = await res.json()
+
 await m.reply('_*sabar banh lagi diprosses klo gagal ra peduli :v...*_')
 conn.sendFile(m.chat, json.result[0].download, 'fb.mp4', author, m)
 }
