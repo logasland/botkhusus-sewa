dca53aa4e27d1fe69951d2fa6367cc5e56aae5bf
logasland <62803820+logasland@users.noreply.github.com>
2022-10-30 06:38:40 +0700

    Update and rename _pesanbang.js to _ara ara.js

--- plugins/_ara ara.js
+++ plugins/_ara ara.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let handler = async(m, { conn }) => {
 conn.sendFile(m.chat, fs.readFileSync('./media/AUD-20220912-WA0046.mp3'), '', '', m, true)
 }
-handler.customPrefix = /(Pesan)/i
+handler.customPrefix = /(Ara|ara|)/i
 handler.command = new RegExp
 
 module.exports = handler
