1ecaaab91e8e2ea9ac8206e4001cd58f6155d9d6
logasland <62803820+logasland@users.noreply.github.com>
2022-11-08 20:36:11 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,17 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 	if (!text) throw `*Link tiktok nya mana?*\n\n*contoh:* \n_${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/_`
-	let res = await fetch(`https://itztobz.me/api/tiktok?url=` + text)
+	let res = await fetch(`https://saipulanuar.ga/api/download/tiktok2?url=` + text + `&apikey=TcFrwf4v`)
+	let name = await conn.getName(m.sender)
 	let json = await res.json()
 
-conn.sendMessage(m.chat, {
-        video: await(await fetch(json.result.no_watermark)).buffer(),
-        caption: `*Link:* ${json.result.no_watermark}`,
-        buttons: [
-          {buttonId: `.get ${json.result.watermark}`, buttonText: {displayText: 'WATERMARK'}, type: "RESPONSE"},
-        ],
-        headerType: 'VIDEO'
-  }, { quoted: m })
+conn.sendFile(m.chat, json.result.video.link1, 'fb.mp4', `dah jadi nich!!!\nceban aja kak ${name}`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
@@ -21,5 +15,3 @@ handler.group = true
 handler.command = /^(tt|tiktok|tik)$/i
 
 module.exports = handler
-
-
