c86819e30a6718a20ef312fadf3cebb1e6398d7f
logasland <62803820+logasland@users.noreply.github.com>
2022-11-20 22:01:19 +0700

    Update you-tube.js

--- plugins/you-tube.js
+++ plugins/you-tube.js
@@ -2,16 +2,15 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 	if (!text) throw `*Link Youtube nya mana kak ${name}?*\n\n*contoh:https://youtu.be/oa04wbLyENo`
-	let res = await fetch(`await fetch(`https://saipulanuar.ga/api/download/ytmp4?url=` + text + `&apikey=TcFrwf4v`)
+	let res = await fetch(`await fetch(`https://nxnapi.herokuapp.com/api/yt/play?judul=xxxtentaction-Everybody%20Dies%20In%20Their%20Nightmares` + text + `&apikey=logasland`)
 	let json = await res.json()
 
-await m.reply(`*_Sabar ${name} lagi di proses.._*`)
 conn.sendMessage(m.chat, {
-        video: await(await fetch(json.result.url)).buffer(),
+        video: await(await fetch(json.result[0].url)).buffer(),
         caption: `\n*Judul video :* ${json.title}\n*dari channel :* ${json.result.channel}\n*publish :* ${json.result.published}\n*jumlah yg nonton😱 : ${json.result.views}\n_${wm}_`,
         footer: 'jangan lupa subrek',
         buttons: [
-          {buttonId: `.yta ${text}`, buttonText: {displayText: 'Mp3'}, type: "RESPONSE"},
+          {buttonId: `${json.result[1].audio.url}`, buttonText: {displayText: 'Mp3'}, type: "RESPONSE"},
         ],
         headerType: 'IMAGE'
   }, { quoted: m })
